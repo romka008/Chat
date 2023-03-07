f346ef9624c084b573fa51a3e2668f134ebbaf3b
Roman <romka008@yandex.ru>
2023-03-07 08:45:43 +0300

    Исправлена сборка проекта, добавлен шаблон для head

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static("./dist/"));
+app.use(express.static("dist"));
 
-app.listen(PORT, function () {
-    console.log(`Example app listening on port ${PORT}!`);
+app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}`);
 });
